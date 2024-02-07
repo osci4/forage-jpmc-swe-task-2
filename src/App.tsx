8bae00de325ac1bc8eab22211873ca4e7faf8f76
Lumbani <lumbani1999@gmail.com>
2024-02-07 21:59:54 +0000

    Change to Graph

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ class App extends Component<{}, IState> {
    */
   renderGraph() {
     if (this.state.showGraph) {
-    return (<Graph data={this.state.data}/>)
+      return (<Graph data={this.state.data}/>)
     }
   }
 
